5bb5a411611f08d814e83b0a2701e9e3a717dd47
Stefane Fermigier <sf@fermigier.com>
2023-07-27 19:24:44 +0200

    feat: add a basic "main" commands.

--- src/prescrypt/main.py
+++ src/prescrypt/main.py
@@ -1,3 +1,35 @@
-def compile(code):
+import ast
+import sys
+from pathlib import Path
+from typing import cast
+
+from prescrypt.expr_compiler import ExpressionCompiler
+from prescrypt.passes.desugar import desugar
+from prescrypt.utils import flatten
+
+
+# Temp compiler class
+class Compiler(ExpressionCompiler):
+
+    def gen_stmt(self, node) -> str:
+        """Not needed"""
+
+    def compile(self, expression: str) -> str:
+        tree = ast.parse(expression)
+        tree = cast(ast.Module, desugar(tree))
+        expr = cast(ast.expr, tree.body[0].value)
+        js_code = self.gen_expr(expr)
+        return flatten(js_code)
+
+
+def py2js(code) -> str:
     compiler = Compiler()
-    compiler.compile(code)
+    return compiler.compile(code)
+
+
+def main():
+    src_path = Path(sys.argv[1])
+    src = src_path.read_text()
+    dst = py2js(src).strip()
+    dst_path = src_path.with_suffix(".js")
+    dst_path.write_text(dst)
