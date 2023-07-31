7e21171300c27c43bd048e4f8a63ad03374c97af
Stefane Fermigier <sf@fermigier.com>
2023-07-31 15:05:52 +0200

    checkpoint: expressions working (mostly).

--- src/prescrypt/codegen_plum/compiler.py
+++
@@ -1,9 +0,0 @@
-from . import gen_expr
-
-
-class FakeCompiler:
-    def gen_expr(self, expr):
-        return gen_expr(expr)
-
-
-compiler = FakeCompiler()
--- src/prescrypt/stdlib_py/functions.py
+++ src/prescrypt/stdlib_py/functions.py
@@ -2,16 +2,17 @@ from ..ast import ast
 from ..exceptions import JSError
 from ..utils import flatten, unify
 
+from prescrypt.codegen.main import CodeGen
 
 #
 # Other builtin functions
 #
-def function_isinstance(compiler, args, kwargs):
+def function_isinstance(codegen: CodeGen, args, kwargs):
     if len(args) != 2:
         raise JSError("isinstance() expects two arguments.")
 
-    ob = unify(compiler.gen_expr(args[0]))
-    cls = unify(compiler.gen_expr(args[1]))
+    ob = unify(codegen.gen_expr(args[0]))
+    cls = unify(codegen.gen_expr(args[1]))
     if cls[0] in "\"'":
         cls = cls[1:-1]  # remove quotes
 
@@ -69,114 +70,114 @@ def function_isinstance(compiler, args, kwargs):
         return ob, " instanceof ", cmp
 
 
-def function_issubclass(compiler, args, kwargs):
+def function_issubclass(codegen: CodeGen, args, kwargs):
     # issubclass only needs to work on custom classes
     if len(args) != 2:
         raise JSError("issubclass() expects two arguments.")
 
-    cls1 = unify(compiler.gen_expr(args[0]))
-    cls2 = unify(compiler.gen_expr(args[1]))
+    cls1 = unify(codegen.gen_expr(args[0]))
+    cls2 = unify(codegen.gen_expr(args[1]))
     if cls2 == "object":
         cls2 = "Object"
     return f"({cls1}.prototype instanceof {cls2})"
 
 
-def function_print(compiler, args, kwargs):
+def function_print(codegen: CodeGen, args, kwargs):
     # Process keywords
     sep, end = '" "', ""
     for kw in kwargs:
         if kw.name == "sep":
-            sep = flatten(compiler.gen_expr(kw.value))
+            sep = flatten(codegen.gen_expr(kw.value))
         elif kw.name == "end":
-            end = flatten(compiler.gen_expr(kw.value))
+            end = flatten(codegen.gen_expr(kw.value))
         elif kw.name in ("file", "flush"):
             raise JSError("print() file and flush args not supported")
         else:
             raise JSError(f"Invalid argument for print(): {kw.name!r}")
 
     # Combine args
-    js_args = [unify(compiler.gen_expr(arg)) for arg in args]
+    js_args = [unify(codegen.gen_expr(arg)) for arg in args]
     end = f" + {end}" if (js_args and end and end != "\n") else ""
     combiner = f" + {sep} + "
     args_concat = combiner.join(js_args) or '""'
     return "console.log(" + args_concat + end + ")"
 
 
-def function_len(compiler, args, kwargs):
+def function_len(codegen: CodeGen, args, kwargs):
     match args:
         case [arg]:
-            js_arg = unify(compiler.gen_expr(arg))
+            js_arg = unify(codegen.gen_expr(arg))
             return f"{js_arg}.length"
         case _:
             raise JSError("len() needs exactly one argument")
 
 
-def function_max(compiler, args, kwargs):
+def function_max(codegen: CodeGen, args, kwargs):
     match args:
         case []:
             raise JSError("max() needs at least one argument")
         case [arg]:
-            js_arg = flatten(compiler.gen_expr(arg))
+            js_arg = flatten(codegen.gen_expr(arg))
             return f"Math.max.apply(null, {js_arg})"
         case [*_]:
-            js_args = ", ".join([unify(compiler.gen_expr(arg)) for arg in args])
+            js_args = ", ".join([unify(codegen.gen_expr(arg)) for arg in args])
             return f"Math.max({js_args})"
 
 
-def function_min(compiler, args, kwargs):
+def function_min(codegen: CodeGen, args, kwargs):
     match args:
         case []:
             raise JSError("min() needs at least one argument")
         case [arg]:
-            js_arg = flatten(compiler.gen_expr(arg))
+            js_arg = flatten(codegen.gen_expr(arg))
             return f"Math.min.apply(null, {js_arg})"
         case [*_]:
-            js_args = ", ".join([unify(compiler.gen_expr(arg)) for arg in args])
+            js_args = ", ".join([unify(codegen.gen_expr(arg)) for arg in args])
             return f"Math.min({js_args})"
 
 
-def function_callable(compiler, args, kwargs):
+def function_callable(codegen: CodeGen, args, kwargs):
     match args:
         case [arg]:
-            js_arg = unify(compiler.gen_expr(arg))
+            js_arg = unify(codegen.gen_expr(arg))
             return f'(typeof {js_arg} === "function")'
         case _:
             raise JSError("callable() needs exactly one argument")
 
 
-def function_chr(compiler, args, kwargs) -> str:
+def function_chr(codegen: CodeGen, args, kwargs) -> str:
     match args:
         case [arg]:
-            js_arg = flatten(compiler.gen_expr(arg))
+            js_arg = flatten(codegen.gen_expr(arg))
             return f"String.fromCharCode({js_arg})"
         case _:
             raise JSError("chr() needs exactly one argument")
 
 
-def function_ord(compiler, args, kwargs) -> str:
+def function_ord(codegen: CodeGen, args, kwargs) -> str:
     match args:
         case [arg]:
-            js_arg = flatten(compiler.gen_expr(arg))
+            js_arg = flatten(codegen.gen_expr(arg))
             return f"{js_arg}.charCodeAt(0)"
         case _:
             raise JSError("ord() exactly one argument")
 
 
-def function_range(compiler, args, kwargs):
+def function_range(codegen: CodeGen, args, kwargs):
     match args:
         case [a]:
             args = [ast.Num(0), a, ast.Num(1)]
-            return call_std_function("range", args)
+            return codegen.call_std_function("range", args)
         case [a, b]:
             args = [a, b, ast.Num(1)]
-            return call_std_function("range", args)
+            return codegen.call_std_function("range", args)
         case [_a, _b, _c]:
-            return call_std_function("range", args)
+            return codegen.call_std_function("range", args)
         case _:
             raise JSError("range() needs 1, 2 or 3 arguments")
 
 
-def function_sorted(compiler, args, kwargs):
+def function_sorted(codegen: CodeGen, args, kwargs):
     if len(args) != 1:
         raise JSError("sorted() needs one argument")
 
@@ -189,4 +190,4 @@ def function_sorted(compiler, args, kwargs):
         else:
             raise JSError(f"Invalid keyword argument for sorted: {kw.name!r}")
 
-    return call_std_function("sorted", [args[0], key, reverse])
+    return codegen.call_std_function("sorted", [args[0], key, reverse])
