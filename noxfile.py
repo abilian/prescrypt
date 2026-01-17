003b4a23b184b4f38797d37039ee26130fc68ecd
Stefane Fermigier <sf@abilian.com>
2026-01-17 11:26:03 +0100

    chore: simplify noxfile

--- noxfile.py
+++ noxfile.py
@@ -3,20 +3,22 @@ from __future__ import annotations
 import nox
 
 PYTHONS = ["3.10", "3.11", "3.12", "3.13", "3.14"]
-# nox.options.sessions = ["lint", "pytest"]
 
 
 @nox.session(python=PYTHONS)
 def tests(session: nox.Session):
-    # Note: we use 'uv' instead of 'pip' to make setup quicker
-    # '--active' and 'external=True' are needed for proper setup
-    session.run("uv", "sync", "--active", external=True)
-    session.run("uv", "run", "--active", "pytest", external=True)
+    uv_sync(session)
+    session.run("pytest")
 
 
 @nox.session(python=PYTHONS[0])
 def lint(session: nox.Session):
-    # Note: we use 'uv' instead of 'pip' to make setup quicker
-    # '--active' and 'external=True' are needed for proper setup
-    session.run("uv", "sync", "--active", external=True)
-    session.run("uv", "run", "--active", "ruff", "check", external=True)
+    uv_sync(session)
+    session.run("ruff", "check")
+
+
+#
+# Utils
+#
+def uv_sync(session: nox.Session):
+    session.run("uv", "sync", "--all-groups", "--all-extras", "--active", external=True)
